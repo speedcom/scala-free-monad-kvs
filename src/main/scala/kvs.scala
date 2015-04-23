a3dacc51808df2776a6bd3a53f497d57f56d557d
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-04-23 21:33:59 +0200

    recipse of project

---
+++ src/main/scala/kvs.scala
@@ -0,0 +1,15 @@
+
+import scalaz._
+import Scalaz._
+import Free._
+
+// This example is based off the one in Runar Bjarnason's "Dead Simple Dependency Injection" talk.
+// http://www.youtube.com/watch?v=ZasXwtTRkio
+
+// 1. ADT
+// 2. Functor definition
+// 3. Lifting functions
+// 4. Composite functions
+// 5. Write scripts
+// 6. Interpreters
+
